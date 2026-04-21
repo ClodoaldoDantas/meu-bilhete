543207f60bde9c47326edc7c3819167af50ab4cb
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2026-04-21 13:08:04 -0300

    refactor: remove unused components and update styles for Home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Meu Bilhete" },
+    { name: "description", content: "Crie sua cartinha de amor para alguém especial" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <h1>Hello World</h1>
 }
